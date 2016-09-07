3cbf411e3b5858e43bd52718d6d6bdf5e7f13b87
akt <akt@pytenlabs.com>
2016-09-07 10:10:17 +0530

    ignore debug file

---
+++ extime.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	args := os.Args[1:]
+	var mArgs string
+	if len(args) == 0 {
+		return
+	}
+
+	for i := 1; i < len(args); i++ {
+		mArgs = mArgs + string(args[i]) + " "
+	}
+
+	tStart := time.Now()
+	out, err := exec.Command(args[0], strings.TrimSpace(mArgs)).Output()
+	tEnd := time.Now()
+
+	if err == nil {
+		tDiff := tEnd.Sub(tStart)
+		fmt.Println(string(out))
+		fmt.Println()
+		fmt.Println("Execution time: " + tDiff.String())
+	} else {
+		fmt.Println("Error executing command")
+		fmt.Println(err)
+	}
+
+}
